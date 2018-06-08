3fe5b89455360ba10d78691df4e1c1f0ae26a951
AMSolovyev <alexander.solovyev7@gmail.com>
2018-06-08 19:01:46 +0300

    I have checked the script with pep8, renamed the arguments

--- github_trending.py
+++ github_trending.py
@@ -1,22 +1,27 @@
 import requests
 import datetime
 
-number_top_repositories = 20
-days_ago = 7
 
-def get_trending_repositories(top_size, exactly_day):
+def get_trending_repositories(top_size, exactly_days):
     url = 'https://api.github.com/search/repositories'
-    period = (datetime.date.today() - datetime.timedelta(exactly_day))
-    load_repositories = {'q':'created:>{}'.format(period), 'sort': 'stars'}
-    repositories = requests.get(url,params=load_repositories).json()
+    period_date = (
+         datetime.date.today() -
+         datetime.timedelta(exactly_days)).isoformat()
+    search_repositories = {
+        'q': 'created : > {}'.format(period_date), 'sort': 'stars'
+    }
+    repositories = requests.get(url, params=search_repositories).json()
     return repositories['items'][:top_size]
 
 if __name__ == '__main__':
-    top_repositories = get_trending_repositories(number_top_repositories, days_ago)
-    print('The top {} repositories are:     ')
-    for repository in top_repositories:
+    count_of_repositiries = 20
+    days = 7
+    most_trending_repository = get_trending_repositories(
+        count_of_repositiries, days
+    )
+    for repository in most_trending_repository:
+        print('The most 20 trending_repositories are:')
         print('Url:', repository['html_url'],
-              'Stars:', repository['stargazers_count'],
-              'open_issues', repository['open_issues'])
-
-
+               'Stars:', repository['stargazers_count'])
+        print('Url:', repository['html_url'],
+              'open_issues:', repository['open_issues'])
